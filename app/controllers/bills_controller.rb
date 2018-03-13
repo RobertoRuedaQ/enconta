36f093ec90c842460b1bff17101feb05e18326c1
rueda.roberto89 <rueda.roberto89@gmail.com>
2018-03-13 08:23:18 -0500

    create controller bills, add HTTParty

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,4 +1,10 @@
 class BillsController < ApplicationController
+  include HTTParty
+  base_uri "34.209.24.195/facturas" 
+  
   def index
+  	@response = HTTParty.get("http://34.209.24.195/facturas\?id\=#{params[:id]}\&start\=2017-01-01\&finish\=2017-01-15
+")
   end
+
 end
